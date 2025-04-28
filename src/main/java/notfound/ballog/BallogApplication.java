75ab9fd06216a064f148dfdbb03ead4b38faf56f
김가희 <gmlgml5023@naver.com>
2025-04-28 15:19:41 +0900

    [init] Reset mastser branch

---
+++ src/main/java/notfound/ballog/BallogApplication.java
@@ -0,0 +1,13 @@
+package notfound.ballog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/notfound/ballog/BallogApplicationTests.java
@@ -0,0 +1,13 @@
+package notfound.ballog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
