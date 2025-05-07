811e102820716dadc7f2f19cde022febb49c61ef
김가희 <gmlgml5023@naver.com>
2025-05-07 11:52:04 +0900

    feat: 좋아요한 영상 화면 라우트 추가
    S12P31A404-208

--- frontend/mobile/src/main/java/com/ballog/mobile/navigation/AppNavHost.kt
+++ frontend/mobile/src/main/java/com/ballog/mobile/navigation/AppNavHost.kt
@@ -24,10 +24,9 @@ import com.ballog.mobile.ui.team.TeamKickScreen
 import com.ballog.mobile.ui.team.TeamListScreen
 import com.ballog.mobile.ui.team.TeamSettingScreen
 
-
 @Composable
 fun AppNavHost(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Routes.ONBOARDING) {
+    NavHost(navController = navController, startDestination = Routes.MYPAGE) { // ✅ 진입 지점 임시 변경
         composable(Routes.ONBOARDING) {
             OnboardingScreen(
                 onEmailLoginClick = {
@@ -39,14 +38,11 @@ fun AppNavHost(navController: NavHostController) {
         composable(Routes.LOGIN) {
             LoginScreen(
                 onLoginClick = {
-                    // TODO: 실제 로그인 성공 시 메인 화면으로 이동
                     navController.navigate(Routes.MAIN) {
-                        popUpTo(Routes.LOGIN) { inclusive = true } // 로그인 화면을 백스택에서 제거
+                        popUpTo(Routes.LOGIN) { inclusive = true }
                     }
                 },
-                onForgotPasswordClick = {
-                    // TODO: 비밀번호 찾기 화면으로 이동
-                },
+                onForgotPasswordClick = { },
                 onSignUpNavigate = { email, password ->
                     navController.navigate("${Routes.SIGNUP}/$email/$password") {
                         popUpTo(Routes.LOGIN) { inclusive = true }
@@ -64,53 +60,33 @@ fun AppNavHost(navController: NavHostController) {
         ) { backStackEntry ->
             val email = backStackEntry.arguments?.getString("email") ?: ""
             val password = backStackEntry.arguments?.getString("password") ?: ""
-            SignupScreen(
-                initialEmail = email,
-                initialPassword = password,
-                navController = navController
-            )
+            SignupScreen(initialEmail = email, initialPassword = password, navController = navController)
         }
 
         composable(
             route = Routes.SIGNUP_EMAIL_VERIFICATION,
-            arguments = listOf(
-                navArgument("email") { type = NavType.StringType }
-            )
+            arguments = listOf(navArgument("email") { type = NavType.StringType })
         ) { backStackEntry ->
             val email = backStackEntry.arguments?.getString("email") ?: ""
-            SignupVerificationScreen(
-                navController = navController,
-                email = email
-            )
+            SignupVerificationScreen(navController = navController, email = email)
         }
 
-        composable(
-            route = Routes.SIGNUP_NICKNAME,
-            // 키보드 상태 유지를 위한 옵션
-            arguments = listOf()
-        ) {
+        composable(Routes.SIGNUP_NICKNAME) {
             SignupNicknameScreen(navController = navController)
         }
 
-        composable(
-            route = Routes.SIGNUP_BIRTHDAY,
-            // 키보드 상태 유지를 위한 옵션
-            arguments = listOf()
-        ) {
+        composable(Routes.SIGNUP_BIRTHDAY) {
             SignupBirthScreen(navController = navController)
         }
 
-        // 프로필 이미지 등록 화면 추가
         composable(Routes.SIGNUP_PROFILE_IMAGE) {
             SignupProfileScreen(navController = navController)
         }
 
-        // 메인 화면 추가
         composable(Routes.MAIN) {
             MainScreen(navController = navController)
         }
 
-        // 마이페이지 관련 화면 추가
         composable(Routes.MYPAGE) {
             MyPageScreen(navController = navController)
         }
@@ -119,129 +95,78 @@ fun AppNavHost(navController: NavHostController) {
             ProfileEditScreen(navController = navController)
         }
 
-//        composable(Routes.MYPAGE_LIKED_VIDEO) {
-//            LikedVideosScreen(navController = navController)
-//        }
+        composable(Routes.MYPAGE_LIKED_VIDEOS) {
+            LikedVideosScreen()
+        }
 
-        // HOME(메인) 화면 추가
         composable(Routes.TEAM_LIST) {
             TeamListScreen(navController = navController)
         }
 
-        // 팀 생성 화면 추가
         composable(Routes.TEAM_CREATE) {
             TeamCreateScreen(
-                onNavigateBack = {
-                    navController.popBackStack()
-                },
-                onClose = {
-                    navController.popBackStack()
-                }
+                onNavigateBack = { navController.popBackStack() },
+                onClose = { navController.popBackStack() }
             )
         }
 
-        // 팀 상세 화면 추가
         composable(
             route = Routes.TEAM_DETAIL,
-            arguments = listOf(
-                navArgument("teamName") { type = NavType.StringType }
-            )
+            arguments = listOf(navArgument("teamName") { type = NavType.StringType })
         ) { backStackEntry ->
             val teamName = backStackEntry.arguments?.getString("teamName") ?: ""
             TeamDetailScreen(
                 navController = navController,
                 teamName = teamName,
-                onBackClick = {
-                    navController.popBackStack()
-                },
-                onSettingClick = {
-                    navController.navigate("team/settings/$teamName")
-                }
+                onBackClick = { navController.popBackStack() },
+                onSettingClick = { navController.navigate("team/settings/$teamName") }
             )
         }
 
-        // 팀 설정 화면 추가
         composable(
             route = Routes.TEAM_SETTINGS,
-            arguments = listOf(
-                navArgument("teamName") { type = NavType.StringType }
-            )
+            arguments = listOf(navArgument("teamName") { type = NavType.StringType })
         ) { backStackEntry ->
             val teamName = backStackEntry.arguments?.getString("teamName") ?: ""
             TeamSettingScreen(
                 navController = navController,
                 teamName = teamName,
-                onBackClick = {
-                    navController.popBackStack()
-                },
-                onCloseClick = {
-                    navController.popBackStack()
-                },
-                onDelegateClick = {
-                    navController.navigate("team/delegate/$teamName")
-                },
-                onKickMemberClick = {
-                    navController.navigate("team/kick/$teamName")
-                },
-                onInviteLinkClick = {
-                    // TODO: Show invite link dialog
-                },
-                onDeleteTeamClick = {
-                    // TODO: Show delete team confirmation dialog
-                },
-                onLeaveTeamClick = {
-                    // TODO: Show leave team confirmation dialog
-                }
+                onBackClick = { navController.popBackStack() },
+                onCloseClick = { navController.popBackStack() },
+                onDelegateClick = { navController.navigate("team/delegate/$teamName") },
+                onKickMemberClick = { navController.navigate("team/kick/$teamName") },
+                onInviteLinkClick = { /* TODO */ },
+                onDeleteTeamClick = { /* TODO */ },
+                onLeaveTeamClick = { /* TODO */ }
             )
         }
 
-        // 팀 권한 위임 화면 추가
         composable(
             route = Routes.TEAM_DELEGATE,
-            arguments = listOf(
-                navArgument("teamName") { type = NavType.StringType }
-            )
+            arguments = listOf(navArgument("teamName") { type = NavType.StringType })
         ) { backStackEntry ->
             val teamName = backStackEntry.arguments?.getString("teamName") ?: ""
             TeamDelegateScreen(
                 navController = navController,
                 teamName = teamName,
-                onBackClick = {
-                    navController.popBackStack()
-                },
-                onCloseClick = {
-                    navController.popBackStack()
-                },
-                onSaveClick = {
-                    // TODO: Implement save logic
-                    navController.popBackStack()
-                }
+                onBackClick = { navController.popBackStack() },
+                onCloseClick = { navController.popBackStack() },
+                onSaveClick = { navController.popBackStack() }
             )
         }
 
-        // 팀 멤버 강제 퇴장 화면 추가
         composable(
             route = Routes.TEAM_KICK,
-            arguments = listOf(
-                navArgument("teamName") { type = NavType.StringType }
-            )
+            arguments = listOf(navArgument("teamName") { type = NavType.StringType })
         ) { backStackEntry ->
             val teamName = backStackEntry.arguments?.getString("teamName") ?: ""
             TeamKickScreen(
                 navController = navController,
                 teamName = teamName,
-                onBackClick = {
-                    navController.popBackStack()
-                },
-                onCloseClick = {
-                    navController.popBackStack()
-                },
-                onSaveClick = {
-                    // TODO: Implement save logic
-                    navController.popBackStack()
-                }
+                onBackClick = { navController.popBackStack() },
+                onCloseClick = { navController.popBackStack() },
+                onSaveClick = { navController.popBackStack() }
             )
         }
-
     }
 }
