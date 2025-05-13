0fbb77466bec58a578ac76a3315809fe0132d40a
nangdev <zovl5546@naver.com>
2025-05-13 15:49:04 +0900

    feat: 팀 도메인 에러 처리 구현
    S12P31A404-268

--- backend/src/main/java/notfound/ballog/domain/team/request/TeamInfoUpdateRequest.java
+++ backend/src/main/java/notfound/ballog/domain/team/request/TeamInfoUpdateRequest.java
@@ -1,5 +1,6 @@
 package notfound.ballog.domain.team.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ public class TeamInfoUpdateRequest {
 
     private Integer teamId;
 
+    @NotBlank(message = "팀 이름을 입력해주세요")
     private String teamName;
 
     private LocalDate foundationDate;
